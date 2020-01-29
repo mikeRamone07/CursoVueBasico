44e238042c9cbf7ccdc4644f426ab1f09843d3f8
Miguel Angel Escalera Garcia <maescalera@firstmajestic.com>
2020-01-29 09:43:32 -0600

    primer comiit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from "vue";
 import App from "./App.vue";
+import "@/assets/css/tailwind.css";
+import Chart from 'chart.js';
+import Chartick from "vue-chartkick";
+import { VueSpinners } from "@saeris/vue-spinners";
 
+import router from "@/router";
+import { dollarFilter, percentFilter } from "@/filters";
 Vue.config.productionTip = false;
+Vue.filter("dollar", dollarFilter);
+Vue.filter("percent", percentFilter);
+
+Vue.use(Chartick.use(Chart));
+Vue.use(VueSpinners);
+
 
 new Vue({
+  router: router,
   render: h => h(App)
 }).$mount("#app");
---
+++ src/router.js
@@ -0,0 +1,36 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+import Home from "@/views/Home";
+import about from "@/views/About";
+import Error from "@/views/Error";
+import CoinDetail from "@/views/CoinDetail";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: about
+    },
+    {
+      path: "*",
+      name: "Error",
+      component: Error
+    },
+    {
+      path: "/coin/:id",
+      name: "coin-detail",
+      component: CoinDetail
+    }
+  ]
+});
